2945cd25102da7ef6429d19248aa08128e0125e6
Chris Mos <chmoser@gmx.net>
2015-11-21 21:45:01 +0100

    update set and comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## 2 functions to store the inverse in external storage (makeCacheMatrix) and
 ## to calculate it if not yet stored (cacheSolve)
 
-## makeCacheMatrix provides a toolset to set, get and matrix in an "external" 
+## makeCacheMatrix provides a toolset to put and get a matrix to and from an "external" 
 ## storage and get and set (store) the inverse of the matrix. 
 ##!! Attention: getinverse returns the value from storage, but does not calculate
 ## it! It is only recalculated whith cacheSolve (see below). If calling data is changed, 
@@ -11,10 +11,10 @@ makeCacheMatrix <- function(x = matrix()) {
     # always create m locally 
     m <- NULL
     # (set is unnecessary for assignment, we only need to save inverse, but not the matrix itself)
-#    set <- function(y) {
-#        x <<- y
-#        m <<- NULL
-#    }
+    set <- function(y) {
+        x <<- y
+           m <<- NULL
+    }
     get <- function() x
     setinverse <- function(inve) m <<- inve
     getinverse <- function() m
